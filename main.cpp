b98c9c4571f96bb4aa998c79cf69b75e815a816b
Tiollit <Tiollit7@gmail.com>
2022-12-03 09:17:11 -0800

    h

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main()
 {
+    int d=0, b=0, c=0;
     srand(time(0));      
+    char new_string[500];
+    cout << "Incert symbols: " << endl;
+    gets_s(new_string);
+    int a = strlen(new_string);
+    for (int i = 0; i < a; i++)
+    {
+        if (isdigit(new_string[i])) d += 1;
+        else if (isalpha(new_string[i])) b += 1;
+        else c += 1;
 
+    }
+    cout << "In string there is " << d << " digits, " << b << "  and " << c << " other symbols" << endl;
     return 0;
 }
