37df6c309c933b2c4cf3a3015c25c24ed86fd839
Tiollit <Tiollit7@gmail.com>
2022-12-03 06:52:30 -0800

    h

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
+
+int pl(int a, int b)
+{
+    return (a + b);
+}
+int min(int a, int b)
+{
+    return (a - b);
+}
+int mn(int a, int b)
+{
+    return (a * b);
+}
+int dil(int a, int b)
+{
+    return (a / b);
+}
 
 int main()
 {
-    srand(time(0));      
+    int a, b, c;
+    cout << "Incert 2 numbers and the operation number: 1 for -, 2 for +, 3 for *, 4 for /" << endl;
+    cin >> a >> b >> c;
+    int (*result[4])(int, int) { pl, min, mn, dil };
+    cout << result[c-1](a,b) << endl;
 
     return 0;
 }
